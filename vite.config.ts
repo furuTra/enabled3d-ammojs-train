29b21832dab2827326f0e65659d1ed8b91e86232
furuTra <tktk3008take@gmail.com>
2023-10-01 16:30:07 +0000

    :tada: init

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/enable3d-ammojs-vite-example/",
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
 });
