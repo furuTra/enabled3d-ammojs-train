2faa0be0372d8cf26a86d785c4620d0378e1b97a
furuTra <tktk3008take@gmail.com>
2023-10-03 17:14:21 +0000

    グループを作成、物理設定のあるオブジェクトの回転を追加

--- src/scene.ts
+++ src/scene.ts
@@ -1,24 +1,25 @@
-import { Project, Scene3D, PhysicsLoader, THREE } from "enable3d";
+import { Project, Scene3D, PhysicsLoader, THREE } from 'enable3d';
 
 class MainScene extends Scene3D {
   box: any;
+  cube: any;
   constructor() {
     //@ts-ignore
-    super("MainScene");
+    super('MainScene');
   }
 
   init() {
-    console.log("Init");
+    console.log('Init');
     this.renderer.setPixelRatio(1);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
   preload() {
-    console.log("Preload");
+    console.log('Preload');
   }
 
   create() {
-    console.log("create");
+    console.log('create');
 
     // Resize window.
     const resize = () => {
@@ -38,35 +39,69 @@ class MainScene extends Scene3D {
     this.warpSpeed();
 
     // enable physics debug
-    this.physics.debug?.enable();
+    // this.physics.debug?.enable();
 
     // position camera
     this.camera.position.set(10, 10, 20);
 
     // blue box
-    this.box = this.add.box({ y: 2 }, { lambert: { color: "deepskyblue" } });
+    this.box = this.add.box({ y: 2, x: -2 }, { lambert: { color: 'deepskyblue' } });
+    this.physics.add.existing(this.box, { mass: 1, collisionFlags: 2 });
 
     // pink box
-    this.physics.add.box({ y: 10 }, { lambert: { color: "hotpink" } });
+    this.physics.add.box({ y: 5, x: 0 }, { lambert: { color: 'hotpink' } });
 
     // green sphere
-    const geometry = new THREE.SphereGeometry(0.8, 16, 16);
-    const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    cube.position.set(0.2, 3, 0);
-    this.scene.add(cube);
-    // add physics to an existing object
-    //@ts-ignore
-    this.physics.add.existing(cube);
+    const sphere = this.add.sphere(
+      { x: 0.2, y: 3, z: 0, radius: 0.8 },
+      { lambert: { color: 0x00ff00, transparent: true, opacity: 0.9 } }
+    );
+    sphere.position.set(0.2, 6, 0);
+    this.physics.add.existing(sphere, { mass: 1, collisionFlags: 2 });
+    sphere.body.setFriction(0);
+
+    const pos = [
+      { y: 0, x: 1.01, z: 0 },
+      { y: 1.01, x: 0, z: 0 },
+      { y: 0, x: 0, z: 1.01 },
+      { y: 0, x: 1.01, z: 1.01 },
+      { y: 1.01, x: 0, z: 1.01 },
+      { y: 1.01, x: 1.01, z: 1.01 },
+    ];
+
+    this.cube = this.add.box(
+      {
+        height: 1,
+        width: 1,
+        depth: 1,
+        y: 10,
+        x: 0,
+        z: 0,
+      },
+      { lambert: { color: 'seagreen', transparent: true, opacity: 0.9 } }
+    );
+    pos.forEach((p) => {
+      const childCube = this.add.box(
+        {
+          height: 1,
+          width: 1,
+          depth: 1,
+          y: p.y,
+          x: p.x,
+          z: p.z,
+        },
+        { lambert: { color: 'seagreen', transparent: true, opacity: 0.9 } }
+      );
+      this.cube.add(childCube);
+    });
+    this.physics.add.existing(this.cube);
   }
 
   update() {
     this.box.rotation.x += 0.01;
     this.box.rotation.y += 0.01;
+    this.box.body.needUpdate = true;
   }
 }
 
-PhysicsLoader(
-  "lib/ammo/kripken",
-  () => new Project({ scenes: [MainScene], antialias: true })
-);
+PhysicsLoader('lib/ammo/kripken', () => new Project({ scenes: [MainScene], antialias: true }));
